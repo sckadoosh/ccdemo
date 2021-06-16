8b2186c215bdbca1bbc374f42029dab584ecb1e3
Shynerl Mascarenhas <shynerl@intuio.io>
2021-06-16 19:16:10 +0530

    port chnaged

--- public_html/index.js
+++ public_html/index.js
@@ -1,6 +1,6 @@
 let express = require( 'express');
 let app = express();
-let port = 8080;
+let port = process.env.path || 8080;
 var path = require('path');
 app.use(express.static('public_html'));
 
