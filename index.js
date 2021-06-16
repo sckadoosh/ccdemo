a0f12a036a663bfafd454c0ad6251064ddfac246
sckadoosh <71373723+sckadoosh@users.noreply.github.com>
2021-06-16 19:56:11 +0530

    index.js uploaded

---
+++ index.js
@@ -0,0 +1,11 @@
+let express = require( 'express');
+let app = express();
+let port = process.env.port || 8080;
+var path = require('path');
+app.use(express.static('public_html'));
+
+
+app.listen(port);
+console.log("Listening on port" + port);
+
+module.exports = app
